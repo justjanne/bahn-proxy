1260dc306d519b4d063ccca9ea92aaf2e8fee3fa
Janne Koschinski <janne@kuschku.de>
2019-12-11 19:29:04 +0100

    Updated build scripts

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-IMAGE=k8r.eu/justjanne/bahn-proxy
-TAGS=$(git describe --always --tags HEAD)
-DEPLOYMENT=bahn-proxy
-POD=bahn-proxy
-NAMESPACE=bahn-tools
-
-kubectl -n $NAMESPACE set image deployment/$DEPLOYMENT $POD=$IMAGE:$TAGS
